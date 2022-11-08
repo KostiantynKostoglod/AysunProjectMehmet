c996edd52e2b3d5264380c1d14da08fde8989a29
kostiantynkostoglod <0675053366@ukr.net>
2022-11-08 06:32:27 -0500

    updatet json added

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+            //    "junit:target/junit/junit-report.xml",
                 "json:target/cucumber.json" ,
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt" ,
